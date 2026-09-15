ca2a7af53681b481e3375969398e0a8dbb499a42
Axel <duquegrimaldo0710@gmail.com>
2026-09-15 18:52:37 -0500

    Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Axel Dashboard',
+    default: 'Axel Dashboard',
+  },
+  description: 'Proyecto del curso Dashboard de Next.js',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
